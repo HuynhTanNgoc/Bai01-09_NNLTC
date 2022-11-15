db1fd8fc15082c38d0eaf031743288508cd58b44
HuynhTanNgoc <117697365+HuynhTanNgoc@users.noreply.github.com>
2022-11-15 09:51:47 +0700

    Add files via upload

---
+++ vd3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("Nhap vao a = ");
+     scanf("%d", &a);
+    printf("\nNhap vao b = ");
+    scanf("%d", &b);
+    int tong = a + b;  
+	printf("%d + %d = %d", a, b, tong);  
+
+}
