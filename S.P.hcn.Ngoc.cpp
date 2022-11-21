a873c68b54bdba038a82080abf80ef39eb3f3297
HuynhTanNgoc <117697365+HuynhTanNgoc@users.noreply.github.com>
2022-11-21 10:14:24 +0700

    Add files via upload

---
+++ S.P.hcn.Ngoc.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int m, n;
+	printf("Chieu dai canh m = ");
+	scanf("%d", &m);
+    printf("Chieu rong canh n = ");
+    scanf("%d", &n);
+    int S = m*n;
+	int P = (m+n)*2;
+	int T = m+n;
+	//S la dien tich hcn, P la chu vi hcn
+	
+    printf("chu vi cua hinh chu nhat la:%d", P);
+    printf("\ndien tich cua hinh chu nhat la:%d", S);
+	printf("\nTong hai canh la %d", T);
+}
