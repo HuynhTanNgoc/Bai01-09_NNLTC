20c0e09df86454b43e03f2b26d1dc12df3039cb1
HuynhTanNgoc <117697365+HuynhTanNgoc@users.noreply.github.com>
2022-12-26 09:51:46 +0700

    Update BT3.2.Ngoc.c

--- BT3.2.Ngoc.c
+++ BT3.2.Ngoc.c
@@ -1,7 +1,13 @@
+//Nhập vào một số nguyên bất kỳ và hiển thị thông báo số đó là chẵn hay lẻ.
 #include<stdio.h>
 int main(){
-    int a;
-    printf("a= ");
-    scanf("%d", &a);
-    printf("%d la so %s", a, (a%2==0)?"Chan": "Le");
+    //Nhap vao so nguyen bat ky:
+    int so_nguyen;
+    printf("Nhap vao so_nguyen = ");
+    scanf("%d", &so_nguyen);
+    //Xu ly va in ra ket qua: 
+    if(so_nguyen %2 == 0) //Neu so_nguyen chia het cho 2 thi do la so chan.
+        printf("%d la so chan", so_nguyen);
+    else //Neu khong chia het cho 2 thi do la so le.
+        printf("%d la so le", so_nguyen);
 }
